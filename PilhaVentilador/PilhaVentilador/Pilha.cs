0f79f9c705f737b68f948e6f8ded0a5546356171
kaualima111 <158177741+kaualima111@users.noreply.github.com>
2024-10-21 17:39:11 -0300

    Add files via upload

---
+++ PilhaVentilador/PilhaVentilador/Pilha.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilhaVentilador
+{
+    public class Pilha
+    {
+        private Stack<string> pecas;
+
+        public Pilha()
+        {
+            pecas = new Stack<string>();
+        }
+
+        // Método para adicionar uma peça à pilha
+        public void AdicionarPeca(string peca)
+        {
+            pecas.Push(peca);
+        }
+
+        // Método para trocar uma peça
+        public void TrocarPeca(string pecaVelha, string pecaNova)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            bool pecaEncontrada = false;
+
+            // Desmontar até encontrar a peça velha
+            while (pecas.Count > 0)
+            {
+                string peca = pecas.Pop();
+                if (peca == pecaVelha)
+                {
+                    // Trocar a peça
+                    tempStack.Push(pecaNova);
+                    pecaEncontrada = true;
+                    break;
+                }
+                else
+                {
+                    tempStack.Push(peca);
+                }
+            }
+
+            // Se a peça velha foi encontrada, reinserir as peças
+            if (pecaEncontrada)
+            {
+                while (tempStack.Count > 0)
+                {
+                    pecas.Push(tempStack.Pop());
+                }
+            }
+            else
+            {
+                Console.WriteLine($"A peça '{pecaVelha}' não foi encontrada.");
+                // Reinserir as peças que foram removidas se a peça não for encontrada
+                while (tempStack.Count > 0)
+                {
+                    pecas.Push(tempStack.Pop());
+                }
+            }
+        }
+
+        // Método para mostrar o estado da pilha
+        public void MostrarPecas()
+        {
+            Console.WriteLine("Peças na pilha:");
+            foreach (var peca in pecas)
+            {
+                Console.WriteLine(peca);
+            }
+        }
+    }
+}
---
+++ PilhaVentilador/PilhaVentilador/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilhaVentilador
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pilha pilha = new Pilha();
+            pilha.AdicionarPeca("Cúpula de Vidro");
+            pilha.AdicionarPeca("Lâmpada");
+            pilha.AdicionarPeca("Hélice");
+            pilha.AdicionarPeca("Suporte");
+
+            Console.WriteLine("Pilha original:");
+            pilha.MostrarPecas();
+
+            // Solicitar ao usuário a peça a ser trocada e a nova peça
+            Console.Write("\nDigite o nome da peça a ser trocada: ");
+            string pecaVelha = Console.ReadLine();
+
+            Console.Write("Digite o nome da nova peça: ");
+            string pecaNova = Console.ReadLine();
+
+            // Trocando a peça
+            pilha.TrocarPeca(pecaVelha, pecaNova);
+
+            Console.WriteLine("\nPilha após troca:");
+            pilha.MostrarPecas();
+            Console.ReadLine();
+        }
+    }
+}
